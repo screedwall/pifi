fea05770fc96bffa29d2e4d5bdda4ce258d55ec0
screedwall <screedwall@gmail.com>
2020-11-08 04:21:25 +0300

    Stage 2 w/o Homework

--- basic/config/web.php
+++ basic/config/web.php
@@ -109,7 +109,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '94.180.130.1'],
     ];
 
     $config['bootstrap'][] = 'gii';
