7227814bcffe9eca04966fbd1d3991069490114e
screedwall <screedwall@gmail.com>
2020-11-28 02:23:41 +0300

    Pay patches

--- basic/controllers/PayController.php
+++ basic/controllers/PayController.php
@@ -220,7 +220,8 @@ class PayController extends Controller
 
 //        $body = '{"TerminalKey":"1605637861944DEMO","OrderId":"34","Success":true,"Status":"CONFIRMED","PaymentId":376057598,"ErrorCode":"0","Amount":199000,"CardId":53417669,"Pan":"430000******0777","ExpDate":"1122","Token":"b98fbc2dd48010459c5c29630c60731eb368a5cbbcf4701695b30b412a81d08f"}';
 
-        $responseObject = json_decode($body, true);
+        return strval($body);
+        $responseObject = json_decode(strval($body), true);
 
         $productjson = "DUMP: ".$responseObject["Token"]."\r\n";
         $jsonfile = \Yii::getAlias('@webroot/Logs.html');
