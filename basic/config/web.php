d2b6403f9bf6bf542d669923f5e6844f6d873a34
screedwall <screedwall@gmail.com>
2020-11-28 02:45:00 +0300

    Pay patches

--- basic/config/web.php
+++ basic/config/web.php
@@ -21,6 +21,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'bKXjN0EcaKZYpIy-xShD1mR6NbWmq4tD',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
