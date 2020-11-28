e0e47b22645c0d098fa5de74401482009c125f14
screedwall <screedwall@gmail.com>
2020-11-28 03:08:07 +0300

    Pay patches

--- basic/controllers/PayController.php
+++ basic/controllers/PayController.php
@@ -226,7 +226,7 @@ class PayController extends Controller
 //        $responseObject = json_decode($body, true);
 
         $jsonfile = \Yii::getAlias('@webroot/Logs.html');
-        $productjson = "BODY: ".print_r($responseObject)."\r\n";
+        $productjson = "BODY: ".var_dump($responseObject)."\r\n";
         $fp = fopen($jsonfile, 'a+');
         fwrite($fp, $productjson."\r\n ========\r\n");
         fclose($fp);
