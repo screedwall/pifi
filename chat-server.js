d2f0cff0335d4835f842edf85d48785cc4cca66f
screedwall <screedwall@gmail.com>
2020-12-17 03:16:52 +0300

    NodeJS fix

--- chat-server.js
+++ chat-server.js
@@ -39,6 +39,7 @@ io.on('connection', (socket) => {
     socket.to(socket.room).emit('user joined', socket.user);
 
     socket.on('chat message', message => {
+        message.replace(/"/g, "'");
         client.lpush(`chat:${socket.room}`, `{"username":"${socket.user}", "message":"${message}"}`, (err, res) => {
             if(res > 20) client.rpop(`chat:${socket.room}`);
             client.expire(`chat:${socket.room}`, 1800);
