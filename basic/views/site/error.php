91f687a67cf13ac2863f99d2a17cf7846e8af61f
screedwall <screedwall@gmail.com>
2021-03-01 02:31:38 +0300

    New error page

--- basic/views/site/error.php
+++ basic/views/site/error.php
@@ -8,22 +8,28 @@
 use yii\helpers\Html;
 
 $this->title = $name;
+
 ?>
 <div class="container">
-    <div class="site-error">
+    <div class="site-error text-center">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <img src="/img/404.png" alt="" class="error404">
+
+        <h3>
+            УПС!
+        </h3>
+
+        <?php if(Yii::$app->response->statusCode != 404): ?>
+        <h4>
+            <p>Кажется у нас тут проблемка, но не переживайте мы всё пофиксим!😉</p>
+            <p>Вы можете ускорить процесс скинув скрин сообщение ниже 👇 администратору</p>
+        </h4>
+        <?php endif; ?>
+
+        <div class="alert alert-danger">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
 </div>
